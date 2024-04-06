abbec528f4b7ebc66aeb6781cefda74ff8037684
green. <41323182+gweeeen@users.noreply.github.com>
2024-04-07 00:02:32 +0200

    oops

--- microfier.py
+++ microfier.py
@@ -17,7 +17,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import json
 import logging
 import time
