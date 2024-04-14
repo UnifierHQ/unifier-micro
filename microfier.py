79c99e9827f3edb2265460a5a3e91e82f2352d64
green. <41323182+gweeeen@users.noreply.github.com>
2024-04-14 12:15:39 +0200

    fix

--- microfier.py
+++ microfier.py
@@ -1,6 +1,6 @@
 """
 Unifier Micro - A much lighter version of Unifier
-Copyright (C) 2024  Green
+Copyright (C) 2024  Green, ItsAsheer
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
